17f3bcf1cceae128f35abb17466fcdb4ad8bf27d
haobayuxi <whaohit@gmail.com>
2023-04-16 15:25:17 +0800

    update

--- hello_world/client.cc
+++ hello_world/client.cc
@@ -11,7 +11,15 @@ erpc::Rpc<erpc::CTransport> *rpc;
 erpc::MsgBuffer req;
 erpc::MsgBuffer resp;
 
-void cont_func(void *, void *) { printf("%s\n", resp.buf_); }
+void cont_func(void *, void *start) {
+  auto msgbuf_idx = reinterpret_cast<system_clock::time_point>(_tag);
+  auto end_time = system_clock::now();
+  auto microseconds_since_epoch =
+      duration_cast<microseconds>(end_time - start_time)
+          .count();  // 将时长转换为微秒数
+  std::cout << microseconds_since_epoch << std::endl;
+  printf("%s\n", resp.buf_);
+}
 
 void sm_handler(int, erpc::SmEventType, erpc::SmErrType, void *) {}
 
@@ -29,14 +37,11 @@ int main() {
   req = rpc->alloc_msg_buffer_or_die(kMsgSize);
   resp = rpc->alloc_msg_buffer_or_die(kMsgSize);
   auto start_time = system_clock::now();
-  rpc->enqueue_request(session_num, kReqType, &req, &resp, cont_func, nullptr);
-  rpc->run_event_loop_once();
-  auto end_time = system_clock::now();
-  auto microseconds_since_epoch =
-      duration_cast<microseconds>(end_time - start_time)
-          .count();  // 将时长转换为微秒数
-  std::cout << microseconds_since_epoch << std::endl;
-  sleep(1000);
+  rpc->enqueue_request(session_num, kReqType, &req, &resp, cont_func,
+                       reinterpret_cast<void *>(start_time));
+  rpc->run_event_loop(1000);
+
+  sleep(1);
 
   delete rpc;
 }
