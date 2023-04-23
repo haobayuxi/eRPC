fe4b762b62577cdd335fc5305133f2a53a711aad
haobayuxi <whaohit@gmail.com>
2023-04-23 14:31:04 +0800

    update

--- hello_world/mem_server/mem_server.cc
+++ hello_world/mem_server/mem_server.cc
@@ -2,11 +2,12 @@
 #include "mem_server.h"
 
 void run_server(MemServer *s, erpc::Nexus *nexus) {
+  printf("thread id = %d\n", s->thread_id);
   erpc::Rpc<erpc::CTransport> rpc(nexus, static_cast<void *>(s), s->thread_id,
                                   NULL);
   s->rpc_ = &rpc;
   while (true) {
-    rpc.run_event_loop_once();
+    rpc.run_event_loop(100);
   }
 }
 
