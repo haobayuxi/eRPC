9bb08241515c44654959d3b524b20c5f24179d9d
haobayuxi <whaohit@gmail.com>
2023-04-23 14:48:27 +0800

    update

--- hello_world/server.cc
+++ hello_world/server.cc
@@ -10,7 +10,8 @@ void reqs_handler(erpc::ReqHandle *req_handle, void *_handler) {
   auto *c = static_cast<MemServer *>(_handler);
   auto &resp = req_handle->pre_resp_msgbuf_;
   c->rpc_->resize_msg_buffer(&resp, kMsgSize);
-  sprintf(reinterpret_cast<char *>(resp.buf_), "hello");
+  sprintf(reinterpret_cast<char *>(resp.buf_),
+          "hello" + std::to_string(c->thread_id));
   // printf("got a message\n");
   c->rpc_->enqueue_response(req_handle, &resp);
 }
