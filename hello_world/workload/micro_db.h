b239a123d257efe081fa41176580718ec0dc9098
haobayuxi <whaohit@gmail.com>
2023-04-23 22:46:46 +0800

    update

--- hello_world/workload/micro_db.h
+++ hello_world/workload/micro_db.h
@@ -12,6 +12,6 @@ class Micro_Db : public DataStore {
   bool get_read_set(ExecutionRes *res) { return true; }
   bool validate_read_set(ValidationRes *res) { return true; }
   bool lock_write_set(ExecutionRes *res) { return true; }
-  bool update_and_release_locks(Commit *c) { return true; }
-  bool release_locks(Abort *abort) { return true; }
+  bool update_and_release_locks(CommitRequest *c) { return true; }
+  bool release_locks(AbortRequest *abort) { return true; }
 };
