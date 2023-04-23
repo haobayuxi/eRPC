ea9e14ebdd8e34a0bc6e9888b771ce2af2296702
haobayuxi <whaohit@gmail.com>
2023-04-23 13:20:34 +0800

    update

--- hello_world/mem_server/mem_server.cc
+++ hello_world/mem_server/mem_server.cc
@@ -5,4 +5,7 @@ void run_server(MemServer *s, erpc::Nexus *nexus) {
   erpc::Rpc<erpc::CTransport> rpc(nexus, static_cast<void *>(&c), thread_id,
                                   basic_sm_handler, port_vec.at(0));
   s->rpc_ = &rpc;
+  while (true) {
+    rpc.run_event_loop_once();
+  }
 }
