40f7cf397f24ecf445239498af850edc73d0cf31
haobayuxi <whaohit@gmail.com>
2023-04-23 15:32:53 +0800

    update

--- hello_world/coordinator/coordinator.h
+++ hello_world/coordinator/coordinator.h
@@ -31,19 +31,4 @@ class Coordinator {
 
 void run_coordinator(Coordinator *c, erpc::Nexus *nexus);
 
-void coordinator_sm_handler(int session_num, erpc::SmEventType sm_event_type,
-                            erpc::SmErrType sm_err_type, void *_context) {
-  auto *c = static_cast<Coordinator *>(_context);
-  c->num_sm_resps++;
-
-  erpc::rt_assert(
-      sm_err_type == erpc::SmErrType::kNoError,
-      "SM response with error " + erpc::sm_err_type_str(sm_err_type));
-
-  if (!(sm_event_type == erpc::SmEventType::kConnected ||
-        sm_event_type == erpc::SmEventType::kDisconnected)) {
-    throw std::runtime_error("Received unexpected SM event.");
-  }
-}
-
 #endif
