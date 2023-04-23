427d584b4a9937c5dbebc9decf9637442a0a315f
haobayuxi <whaohit@gmail.com>
2023-04-23 13:35:21 +0800

    update

--- hello_world/mem_server/mem_server.cc
+++ hello_world/mem_server/mem_server.cc
@@ -2,8 +2,8 @@
 #include "mem_server.h"
 
 void run_server(MemServer *s, erpc::Nexus *nexus) {
-  erpc::Rpc<erpc::CTransport> rpc(nexus, static_cast<void *>(&c), thread_id,
-                                  basic_sm_handler, port_vec.at(0));
+  erpc::Rpc<erpc::CTransport> rpc(nexus, static_cast<void *>(&c), s->thread_id,
+                                  basic_sm_handler);
   s->rpc_ = &rpc;
   while (true) {
     rpc.run_event_loop_once();
--- hello_world/mem_server/mem_server.h
+++ hello_world/mem_server/mem_server.h
@@ -6,8 +6,9 @@ class MemServer {
  public:
   int id;
   erpc::Rpc<erpc::CTransport>* rpc_;
+  size_t thread_id;
 
  private:
-}
+};
 
-void run_server(MemServer * s, erpc::Nexus *nexus);
+void run_server(MemServer* s, erpc::Nexus* nexus);
